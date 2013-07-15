c6956c7498167cbce6e1fd06622f1270a4b38b1e
Philp Feairheller <pfeairheller@gmail.com>
2013-07-14 21:33:21 -0400

    Added app specific configuration structure parsing from JSON config file.

--- configuration.go
+++ configuration.go
@@ -10,16 +10,17 @@ import (
 
 type Configuration struct {
 	ConfigHost string
-	Application map[string]string
+	Application interface {}
 }
 
-func NewConfiguration(fileName string) (c *Configuration) {
+func NewConfiguration(fileName string, appConfig interface{}) (c *Configuration) {
 	c = new(Configuration)
-
+	c.Application = appConfig
 	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
 	dec := json.NewDecoder(file)	
 	err = dec.Decode(c)
